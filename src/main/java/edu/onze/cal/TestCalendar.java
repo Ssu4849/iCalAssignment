776fbc4ed207846cba0272fa8e896a32b03b1061
Sheng <ssu2@hawaii.edu>
2016-03-13 22:30:43 -1000

    Add loops for user input

--- src/main/java/edu/onze/cal/TestCalendar.java
+++ src/main/java/edu/onze/cal/TestCalendar.java
@@ -28,18 +28,22 @@ public class TestCalendar {
 	/**
 	 * @param args
 	 */
-	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		sc = new Scanner(System.in);
+		System.out.println(
+				"Usage: Enter 1 to write, 2 to read a single file, 3 to read all files and calculate great-circle distance");
 
-		if (args.length == 0) {
-			System.err.println("Usage: Enter 1 to write, 2 to read a single file, 4 to read all files and calculate great-circle distance");
+		int choice = -1;
+		try {
+			choice = Integer.parseInt(sc.nextLine());
+		} catch (NumberFormatException e) {
+			System.err.println("Enter a valid number");
 			System.exit(0);
 		}
 
-		if (Integer.parseInt(args[0]) == 1) {
+		if (choice == 1) {
 			createICSFile();
-		} else if (Integer.parseInt(args[0]) == 2) {
+		} else if (choice == 2) {
 			System.out.println("Enter the name of the ICS file you want to read (without extension)");
 			File file = new File(sc.nextLine() + ".ics");
 			try {
@@ -47,7 +51,7 @@ public class TestCalendar {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		} else if (Integer.parseInt(args[0]) == 3) {
+		} else if (choice == 3) {
 			readCalculateCircleDistance();
 		} else {
 			System.err.println(
@@ -129,66 +133,32 @@ public class TestCalendar {
 
 		iCalObj calendar = new iCalObj(file);
 
-		System.out.println("Enter the time the event start (Format: YYYY-MM-DD HH:MM:SS)");
-		String dateTimeStart = sc.nextLine();
-		// Checks timeStart for correct format
-		if (dateTimeStart.split(":").length != 3 || dateTimeStart.split("-").length != 3) {
-			System.err.println("Date/Time start is not the correct format");
-			System.exit(0);
+		String dateTimeStart = "";
+		boolean incorrectFormat = true;
+		while (incorrectFormat) {
+			System.out.println("Enter the time the event start (Format: YYYY-MM-DD HH:MM:SS)");
+			incorrectFormat = false;
+			dateTimeStart = sc.nextLine();
+			if (!validateDatetime(dateTimeStart)) {
+				incorrectFormat = true;
+			}
 		}
 
-		// Parses user input for anything != int
-		try {
-			String date = dateTimeStart.split(" ")[0];
-			for (String s : date.split("-")) {
-				Integer.parseInt(s);
+		String dateTimeEnd = "";
+		incorrectFormat = true;
+		while (incorrectFormat) {
+			System.out.println("Enter the time the event end (Format: YYYY-MM-DD HH:MM:SS)");
+			incorrectFormat = false;
+			dateTimeEnd = sc.nextLine();
+			if (!validateDatetime(dateTimeEnd)) {
+				incorrectFormat = true;
 			}
-		} catch (NumberFormatException e) {
-			System.err.println("Month, date, or year is not a valid integer");
-			System.exit(0);
-		}
-		try {
-			String time = dateTimeStart.split(" ")[1];
-			for (String s : time.split(":")) {
-				Integer.parseInt(s);
-			}
-		} catch (NumberFormatException e) {
-			System.err.println("Hour, second, or time is not a valid integer");
-			System.exit(0);
 		}
 
-		System.out.println("Enter the time the event end (Format: YYYY-MM-DD HH:MM:SS)");
-		String dateTimeEnd = sc.nextLine();
-		// Checks timeEnd for correct format
-		if (dateTimeEnd.split(":").length != 3 || dateTimeEnd.split("-").length != 3) {
-			System.err.println("Date/Time end is not the correct format");
-			System.exit(0);
-		}
-
-		// Parses User input for anything != Int
-		try {
-			String date = dateTimeEnd.split(" ")[0];
-			for (String s : date.split("-")) {
-				Integer.parseInt(s);
-			}
-		} catch (NumberFormatException e) {
-			System.err.println("Month, date, or year is not a valid integer");
-			System.exit(0);
-		}
-		try {
-			String time = dateTimeEnd.split(" ")[1];
-			for (String s : time.split(":")) {
-				Integer.parseInt(s);
-			}
-		} catch (NumberFormatException e) {
-			System.err.println("Hour, second, or time is not a valid integer");
-			System.exit(0);
-		}
-
-		System.out.println("Enter a summary of the event: ");
+		System.out.println("Enter a summary of the event:");
 		String summary = sc.nextLine();
 
-		System.out.println("Enter the location ");
+		System.out.println("Enter the location, or leave it blank");
 		String location = sc.nextLine();
 
 		System.out.println("Enter a description of the event, or leave it blank");
@@ -197,7 +167,42 @@ public class TestCalendar {
 		System.out
 				.println("Enter the Geographic Position of the event: Latitude [space] longitude, or leave it blank \n"
 						+ "	(format: <degrees>,<minutes>,<seconds> <degrees>,<minutes>,<seconds>)");
-		String geoPosition = sc.nextLine();
+		incorrectFormat = true;
+		String geoPosition = "";
+		while (incorrectFormat) {
+			incorrectFormat = false;
+			geoPosition = sc.nextLine();
+			// validates geographic position format
+			if (geoPosition.trim().length() != 0) {
+				String[] latLongArray = geoPosition.split(" ");
+				if (latLongArray.length != 2) {
+					System.err.println("Geographic position is not the correct format");
+					incorrectFormat = true;
+				} else if (latLongArray[0].split(",").length != 3) {
+					System.err.println("Latitude is not the correct format");
+					incorrectFormat = true;
+				} else if (latLongArray[1].split(",").length != 3) {
+					System.err.println("Longitude is not the correct format");
+					incorrectFormat = true;
+				} else {
+					incorrectFormat = false;
+				}
+
+				// Parses User input for anything != a decimal
+				if (!incorrectFormat && geoPosition.trim().length() != 0) {
+					try {
+						for (String latLong : geoPosition.split(" ")) {
+							for (String coordinate : latLong.split(",")) {
+								Double.parseDouble(coordinate);
+							}
+						}
+					} catch (NumberFormatException e) {
+						System.err.println("Geographic position not a decimal");
+						incorrectFormat = true;
+					}
+				}
+			}
+		}
 
 		System.out.println("Enter 1 for the event to be PUBLIC, 2 for PRIVATE, and 3 for CONFIDENTIAL");
 		String classification = sc.nextLine();
@@ -208,7 +213,6 @@ public class TestCalendar {
 				classificationChoice = Integer.parseInt(classification);
 			} catch (NumberFormatException e) {
 				System.err.println("Not a number");
-				System.exit(0);
 			}
 		}
 
@@ -219,35 +223,6 @@ public class TestCalendar {
 			classificationChoice = 1;
 		}
 
-		// validates geographic position format
-		if (geoPosition.trim().length() != 0) {
-			String[] latLongArray = geoPosition.split(" ");
-			if (latLongArray.length != 2) {
-				System.err.println("Geographic position is not the correct format");
-				System.exit(0);
-			}
-			if (latLongArray[0].split(",").length != 3) {
-				System.err.println("Latitude is not the correct format");
-				System.exit(0);
-			}
-			if (latLongArray[1].split(",").length != 3) {
-				System.err.println("Longitude is not the correct format");
-				System.exit(0);
-			}
-
-			// Parses User input for anything != a decimal
-			try {
-				for (String latLong : geoPosition.split(" ")) {
-					for (String coordinate : latLong.split(",")) {
-						Double.parseDouble(coordinate);
-					}
-				}
-			} catch (NumberFormatException e) {
-				System.err.println("Geographic position not a decimal");
-				System.exit(0);
-			}
-		}
-		// Checks that event date/time is the correct format
 		try {
 			Event event1 = calendar.createEvent(dateTimeStart, dateTimeEnd, summary, description, location);
 			if (geoPosition.trim().length() != 0) {
@@ -274,6 +249,7 @@ public class TestCalendar {
 		}
 	}
 
+	@SuppressWarnings("deprecation")
 	public static void readCalculateCircleDistance() {
 		System.out.println("Reading all .ics files in directory...");
 
@@ -346,6 +322,37 @@ public class TestCalendar {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static boolean validateDatetime(String dateTime) {
+		boolean incorrectFormat = false;
+		// Checks timeStart for correct format
+		if (dateTime.split(":").length != 3 || dateTime.split("-").length != 3) {
+			System.err.println("Date/Time start is not the correct format");
+			incorrectFormat = true;
+		}
+		if (!incorrectFormat) {
+			// Parses user input for anything != int
+			try {
+				String date = dateTime.split(" ")[0];
+				for (String s : date.split("-")) {
+					Integer.parseInt(s);
+				}
+			} catch (NumberFormatException e) {
+				System.err.println("Month, date, or year is not a valid integer");
+				incorrectFormat = true;
+			}
+			try {
+				String time = dateTime.split(" ")[1];
+				for (String s : time.split(":")) {
+					Integer.parseInt(s);
+				}
+			} catch (NumberFormatException e) {
+				System.err.println("Hour, second, or time is not a valid integer");
+				incorrectFormat = true;
+			}
+		}
+		return !incorrectFormat;
 	}
 
 }
